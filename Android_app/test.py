b44e3b71a5684e12994faf64b06de4af8b6b1f46
disa-star <53687189+disa-star@users.noreply.github.com>
2023-05-09 14:43:33 +0800

    Update test.py

--- Android_app/test.py
+++ Android_app/test.py
@@ -1,17 +1,27 @@
 import pickle
 
+def func():
+     global a
+     print(f'在func中的global a为{a}')
+
 a = 0
-class test():
+if 1 == 1:
+    #必须要确保 定义的类和即将要加载的类是一致的
+    class test():
         def __init__(self):
             global a
             #pickle会在一开始记住init的部分和self的子属性
             self.a = a
+            global func
+            self.func = func
         def print(self):
             #这里pickle哪怕在没有global的时候都会记住global a
             #并且 必须要是a这个变量才能使用
             global a
             print(f'self.a={self.a}\n     a={a}')
-
+else:
+    class test():
+        pass
 
 if 1 == 0:
     a = 123
@@ -31,3 +41,4 @@ print(ld)
 for a in range(0,5):
     #a = 2
     ld.print()
+ld.func()
