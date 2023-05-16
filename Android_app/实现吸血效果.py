5009e4f2141e31481b5df5a221012b9e802730b5
disa-star <53687189+disa-star@users.noreply.github.com>
2023-05-16 23:16:42 +0800

    Update 实现吸血效果.py

--- Android_app/实现吸血效果.py
+++ Android_app/实现吸血效果.py
@@ -5,14 +5,10 @@ import copy
 #action为02
 #attribution为03
 #unit为04
-#
 attack = res.event()
 after_attack = res.event()
-#
 hp = res.attribution(valuable=True,limit=[0,20],value=10)
 melee = res.attribution(valuable=True,limit=[0,999],value=5)
-
-
 
 def blood_steal_realize(owner,repeat,world_status):
     if world_status['event_id_now'] == after_attack.id:
@@ -20,14 +16,12 @@ def blood_steal_realize(owner,repeat,world_status):
             res.universal_id_dict[owner].attribution_dict[hp.id].add_value(world_status['damage'],world_status)
     return 0,world_status
 blood_steal_atom = res.atom(blood_steal_realize)
-
-
-
 def damage_settlement(owner,repeat,world_status):
     if world_status['event_id_now'] == attack.id:
         res.universal_id_dict[world_status['attackee']].attribution_dict[hp.id].add_value(-world_status['damage'],world_status)
     return 0,world_status
 damage_atom = res.atom(damage_settlement)
+
 do_attack = res.action(reg_atom_dict={attack.id:[damage_atom.id]},event_list=[attack.id,after_attack.id])
 
 player = res.unit(init_attribution = {hp.id:{'value':21,'limit':[0,35],'attach_ctn':1},
@@ -36,9 +30,6 @@ p1 = copy.deepcopy(player)
 p1.after_deepcopy()
 
 blood_steal = res.attribution(valuable=False,reg_atom_dict={after_attack.id:[blood_steal_atom.id]})
-#测试一下after_change_attribution_dict
-
-#player.after_change_attribution_dict()
 p1.after_change_attribution_dict()
 
 p1.attribution_dict[blood_steal.id].attach()
@@ -49,4 +40,3 @@ p1.act(do_attack.id,{'attacker':p1.id,
                      'damage':p1.attribution_dict[melee.id].value})
 print(p1.attribution_dict[hp.id].__dict__)
 print(p2.attribution_dict[hp.id].__dict__)
-del p2
