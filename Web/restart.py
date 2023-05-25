b47d54dff305291fc17417ea5d277a244e767027
Osar00 <109191450+Osar00@users.noreply.github.com>
2023-05-25 10:03:32 +0800

    更新了web wbs

---
+++ Web/restart.py
@@ -0,0 +1,425 @@
+import copy
+universal_id_dict = {}
+attribution_model_dict = {}
+#定义atom末位为00
+#event为01
+#action为02
+#attribution为03
+#unit为04
+class id_control():
+    def __init__(self,id=0):
+        global universal_id_dict
+        assert self.num != None
+        #决定id
+        #正则化id
+        
+        if id <= 0:
+            self.id = self.get_valid_id()
+        else:
+            id = id*100 + self.num
+            if self.id_validation_check(id):
+                #正则化id
+                self.id = id
+            else:
+                self.id = self.get_valid_id()
+        del id
+        universal_id_dict[self.id] = self
+        return self
+    #检验id是否合法
+    def id_validation_check(self,id):
+        if id in universal_id_dict.keys():
+            return False
+        else:
+            return True
+    #获取合法id
+    def get_valid_id(self):
+        for i in range(1,10000):
+            id = i*100 + self.num
+            if self.id_validation_check(id):
+                return id
+            else:
+                pass
+        else:
+            raise "10000以下的id都被使用了,请手动设置id"
+        
+    def __del__(self):
+        del universal_id_dict[self.id]
+
+    def btn_change_description(self,description:dict):
+        self.description = description
+
+    def refresh_id(self):
+        if universal_id_dict[self.id] is not self:
+            self.id = self.get_valid_id()
+            universal_id_dict[self.id] = self
+
+
+class atom(id_control):
+    
+    def __init__(self,func,id=0,*args,**kwargs):
+        global universal_id_dict        
+        #载入描述
+        if 'description' in kwargs.keys():
+            self.description = kwargs['description']
+        #载入挂载函数
+        self.func = func
+        assert isinstance(id,int) 
+        self.num = 0
+        super().__init__(id=id)
+    
+    def btn_rebond(self,func):
+        self.func = func
+    #atom不负责存储注册信息
+    def register_to_event(self,event_id,owner_id=None):
+        universal_id_dict[event_id].atom_list_append(atom_id=self.id,owner_id=owner_id)
+    
+    def resgin_from_event(self,event_id,owner_id=None):
+        universal_id_dict[event_id].atom_list_pop(atom_id=self.id,owner_id=owner_id)
+
+
+
+
+
+
+
+class event(id_control):
+    def __init__(self,id=0,*args,**kwargs):
+        #载入描述
+        if 'description' in kwargs.keys():
+            self.description = kwargs['description']
+        
+        self.atom_list = []
+
+        global universal_id_dict
+        assert isinstance(id,int) 
+        self.num = 1
+        super().__init__(id=id)
+    
+    def atom_list_append(self,atom_id,owner_id):
+        for i,atom_info in enumerate(self.atom_list):
+            if atom_id == atom_info['atom_id'] and owner_id == atom_info['owner_id']:
+                self.atom_list[i]['repeat'] += 1
+                break
+        else:
+            self.atom_list.append({'atom_id':atom_id,
+                                'owner_id':owner_id,
+                                'repeat':1})
+            
+    def atom_list_pop(self,atom_id,owner_id):
+        for i,atom_info in enumerate(self.atom_list):
+            if atom_id == atom_info['atom_id'] and owner_id == atom_info['owner_id']:
+                self.atom_list[i]['repeat'] -= 1
+                break
+        else:
+            pass
+
+        for i,atom_info in enumerate(self.atom_list):
+            if atom_info['repeat'] <= 0:
+                del self.atom_list[i]
+
+    def run(self,world_status:dict):
+        #用于遗传action_id
+        if 'action_id_now' in world_status.keys():
+            action_prev = world_status['action_id_now']
+        else:
+            action_prev = None
+        if 'action_starter' in world_status.keys():
+            starter_prev = world_status['action_starter']
+        else:
+            starter_prev = None      
+            
+        for atom_info in self.atom_list:
+            if action_prev != None:
+                world_status['action_id_now'] = action_prev
+            if starter_prev != None:
+                world_status['action_starter'] = starter_prev
+            world_status['event_id_now'] = self.id
+            ops,world_status = universal_id_dict[atom_info['atom_id']].func(
+                owner=atom_info['owner_id'],
+                repeat=atom_info['repeat'],
+                world_status=world_status)
+            #删除阶段
+            del world_status['event_id_now']
+            if action_prev == None:
+                del world_status['action_id_now']
+            if starter_prev == None:
+                del world_status['action_starter']
+            if ops == 0:
+                pass
+            elif ops == 1:
+                #直接退出这次事件
+                break
+            elif ops >= 2:
+                #不但退出这次事件,并且直接退出上层激发事件列的行动
+                return ops-1,world_status
+        return 0,world_status
+
+    
+
+
+
+
+class attribution(id_control):
+    def __init__(self,id=0,*args,**kwargs):
+        if 'description' in kwargs.keys():
+            self.description = kwargs['description']
+        if 'valuable' in kwargs.keys():
+            self.valuable = kwargs['valuable']
+        else:
+            self.valuable = False
+        if 'reg_atom_dict' in kwargs.keys():
+                self.reg_atom_dict = kwargs['reg_atom_dict']
+        else:
+            self.reg_atom_dict = []
+
+        if self.valuable:
+            if 'limit' in kwargs.keys():
+                self.limit = kwargs['limit']  
+            else:
+                self.limit = [0,0]
+            if 'value' in kwargs.keys():
+                self.value = kwargs['value']
+            else:
+                self.value = 0
+            if 'event_list_on_value_change' in kwargs.keys():
+                self.event_list_on_value_change = kwargs['event_list_on_value_change']
+            else:
+                self.event_list_on_value_change = {}
+            if 'event_list_on_limit_change' in kwargs.keys():
+                self.event_list_on_limit_change = kwargs['event_list_on_limit_change']
+            else:
+                self.event_list_on_limit_change = {}
+        self.owner = None
+        self.attach_ctn = 0
+        self.num = 3
+
+        global universal_id_dict,attribution_model_dict
+        assert isinstance(id,int) 
+        super().__init__(id=id)
+        attribution_model_dict[self.id] = self
+
+    def __del__(self):
+        pass
+
+    def delete(self):
+        del universal_id_dict[self.id]
+        del attribution_model_dict[self.id]
+        del self
+
+    def set_limit(self,lim:list,world_status:dict):
+        assert self.valuable == True
+        world_status['limit_change_attribution'] = self.id
+        world_status['limit_change_unit'] = self.owner
+        world_status['lim_change_to'] = lim
+        for event_id in self.event_list_on_limit_change:
+            ops,world_status = universal_id_dict[event_id].run(world_status)
+            #同样,下面为了接收event run出来的结果
+            if ops == 0:
+                pass
+            elif ops == 1:
+                break
+            elif ops >= 2:
+                universal_id_dict[world_status['limit_change_unit']].attribution_dict[world_status['limit_change_attribution']].set_limit_commit(world_status['lim_change_to'])
+                del world_status['limit_change_attribution'],world_status['limit_change_unit'],world_status['lim_change_to']
+                return ops-1,world_status
+            
+        universal_id_dict[world_status['limit_change_unit']].attribution_dict[world_status['limit_change_attribution']].set_limit_commit(world_status['lim_change_to'])
+        del world_status['limit_change_attribution'],world_status['limit_change_unit'],world_status['lim_change_to']
+        return 0,world_status
+    
+    #套皮函数
+    def change_limit(self,delta:list,world_status:dict):
+        lim = [self.limit[0]+delta[0],self.limit[1]+delta[1]]
+        self.set_limit(lim,world_status)
+
+    def add_value(self,delta:int,world_status:dict):
+        assert self.valuable == True
+        world_status['value_change_attribution'] = self.id
+        world_status['value_change_unit'] = self.owner
+        world_status['value_change_by'] = delta
+        for event_id in self.event_list_on_value_change:
+            ops,world_status = universal_id_dict[event_id].run(world_status)
+            #同样,下面为了接收event run出来的结果
+            if ops == 0:
+                pass
+            elif ops == 1:
+                break
+            elif ops >= 2:
+                universal_id_dict[world_status['value_change_unit']].attribution_dict[world_status['value_change_attribution']].change_value_commit(world_status['value_change_by'])
+                del world_status['value_change_attribution'],world_status['value_change_unit'],world_status['value_change_by']
+                return ops-1,world_status
+            
+        universal_id_dict[world_status['value_change_unit']].attribution_dict[world_status['value_change_attribution']].change_value_commit(world_status['value_change_by'])
+        del world_status['value_change_attribution'],world_status['value_change_unit'],world_status['value_change_by']
+        return 0,world_status
+    
+    #套皮
+    def set_value(self,value:int):
+        delta = value - self.value
+        self.add_value(delta)
+
+    def set_owner(self,owner_id,*args,**kwargs):
+        self.owner = owner_id
+        if self.valuable:
+            if 'value' in kwargs.keys():
+                self.value = kwargs['value']
+            if 'limit' in kwargs.keys():
+                self.limit = kwargs['limit']
+            self.normalization()
+        else:
+            pass
+        if 'attach_ctn' in kwargs.keys():
+                self.attach_to_level(kwargs['attach_ctn']) 
+
+    def normalization(self):
+        if self.value > self.limit[1]:
+            self.value = self.limit[1]
+        elif self.value < self.limit[0]:
+            self.value = self.limit[0]
+
+    #这之下是安全的btn系列函数
+    def set_limit_commit(self,lim:list):
+        self.limit = lim
+        self.normalization()
+
+    def change_limit_commit(self,lim:list):
+        self.limit[0] += lim[0]
+        self.limit[1] += lim[1]
+        self.normalization()
+
+    def set_value_commit(self,value:int):
+        #真正的更新了数值
+        self.value = value
+        #归一化防止溢出
+        self.normalization()
+
+    def change_value_commit(self,delta:int):
+        #真正的更新了数值
+        self.value += delta
+        #归一化防止溢出
+        self.normalization()
+    #这之上是安全的btn系列函数
+
+    def attach(self):
+        #这个和action的注册相似,
+        if self.attach_ctn == 0:
+            for event_id in self.reg_atom_dict:
+                for atom_id in self.reg_atom_dict[event_id]:
+                    universal_id_dict[event_id].atom_list_append(atom_id,self.owner)   
+        else:
+            self.attach_ctn += 1
+
+    def unattach(self):
+        #这个和action的注销相似,
+        self.attach_ctn -= 1
+        if self.attach_ctn <= 0:
+            self.attach_ctn = 0
+            for event_id in self.reg_atom_dict:
+                for atom_id in self.reg_atom_dict[event_id]:
+                    universal_id_dict[event_id].atom_list_pop(atom_id,self.owner) 
+
+    def attach_to_level(self,level):
+        if level<=0:
+            self.attach_ctn = 0
+            self.unattach()
+        else:
+            self.attach()
+            self.attach_ctn = level
+
+    def attach_by_level(self,delta):
+        self.attach_to_level(self.attach_ctn+delta)
+
+    def dict_out_data(self):
+        if self.valuable:
+            return {'value':self.value,
+                    'limit':self.limit,
+                    'attach_ctn':self.attach_ctn}
+
+
+class action(id_control):
+    def __init__(self,id=0,*args,**kwargs):
+        assert isinstance(id,int)
+        self.num = 2
+        if 'description' in kwargs.keys():
+            self.description = kwargs['description']
+        if 'reg_atom_dict' in kwargs.keys():
+            self.reg_atom_dict = kwargs['reg_atom_dict']
+        if 'event_list' in kwargs.keys():
+            self.event_list = kwargs['event_list']
+        #稍后实现在action时获得attribution功能
+        
+        global universal_id_dict
+        super().__init__(id=id)
+
+    def setup(self,owner_id):
+        for event_id in self.reg_atom_dict:
+            for atom_id in self.reg_atom_dict[event_id]:
+                universal_id_dict[event_id].atom_list_append(atom_id,owner_id)
+
+    def setdown(self,owner_id):
+        for event_id in self.reg_atom_dict:
+            for atom_id in self.reg_atom_dict[event_id]:
+                universal_id_dict[event_id].atom_list_pop(atom_id,owner_id)
+
+    def do(self,owner_id,world_status:dict):
+        self.setup(owner_id)
+        for event_id in self.event_list:
+            world_status['action_id_now'] = self.id
+            world_status['action_starter'] = owner_id
+            ops,world_status = universal_id_dict[event_id].run(world_status)
+            del world_status['action_id_now']
+            if ops == 0:
+                pass
+            elif ops == 1:
+                break
+            elif ops >= 2:
+                self.setdown(owner_id)
+                return ops-1,world_status
+        self.setdown(owner_id)
+        return 0,world_status
+
+
+
+class unit(id_control):
+    def __init__(self,id=0,*args,**kwargs):
+        global universal_id_dict
+        global attribution_model_dict
+        assert isinstance(id,int)
+        self.num = 4
+        super().__init__(id=id)
+        if 'description' in kwargs.keys():
+            self.description = kwargs['description']
+        #初始化全部词缀和默认已拥有词缀
+        
+        self.attribution_dict = copy.deepcopy(attribution_model_dict)
+        for id in self.attribution_dict:
+            self.attribution_dict[id].set_owner(self.id)
+        if 'init_attribution' in kwargs:
+            for id in kwargs['init_attribution']:
+                self.attribution_dict[id].set_owner(self.id,**(kwargs['init_attribution'][id]))
+
+        
+    #这个是权宜之计
+    def after_change_attribution_dict(self):
+        temp = copy.deepcopy(attribution_model_dict)
+        for id in temp:
+            temp[id].set_owner(self.id)
+        for id in self.attribution_dict:
+            
+            #转移信息+全部注销一次
+            temp[id].set_owner(self.id,**(self.attribution_dict[id].dict_out_data()))
+            self.attribution_dict[id].attach_to_level(0)
+
+        self.attribution_dict = temp
+        del temp
+
+    def after_deepcopy(self):
+        #如果自己和全局字典的内容不一样
+        self.refresh_id()
+        for id in self.attribution_dict:
+            self.attribution_dict[id].set_owner(self.id)
+
+    def act(self,action_id,world_status):
+        universal_id_dict[action_id].do(self.id,world_status)
+
+    #attach系列函数稍后实现
--- Web/test.py
+++ Web/test.py
@@ -4,6 +4,7 @@ from PIL import Image
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 import restart as res
+import random
 
 # 初始化所需存储状态
 if "uploaded_file" not in st.session_state:
@@ -12,20 +13,25 @@ if "unit_list" not in st.session_state:
     st.session_state["unit_list"] = []
 if "df" not in st.session_state:
     st.session_state["df"] = None
+if "df1" not in st.session_state:
+    st.session_state["df1"] = None
 
 # 关于地图编辑的相关函数
 def drawing():
+    st.subheader('地图编辑')
     drawing_mode = st.sidebar.selectbox(
         "画图工具", ("point", "freedraw", "line", "rect", "circle", "transform")
     )
 
-    stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+    stroke_width = st.sidebar.slider("画笔宽度：", 1, 25, 3)
     if drawing_mode == 'point':
-        point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-    stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-    bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
+        point_display_radius = st.sidebar.slider("点圈半径：", 1, 25, 3)
+    stroke_color = st.sidebar.color_picker("画笔颜色 ")
+    bg_color = st.sidebar.color_picker("背景颜色", "#eee")
     if st.session_state["uploaded_file"] is not None:
-        bg_image = st.session_state["uploaded_file"]
+        bg_image = Image.open(st.session_state["uploaded_file"])
+        w = bg_image.width       #图片的宽
+        h = bg_image.height      #图片的高
 
 
     # 画布设置
@@ -34,9 +40,12 @@ def drawing():
         stroke_width=stroke_width,
         stroke_color=stroke_color,
         background_color=bg_color,
-        background_image=Image.open(bg_image) if st.session_state["uploaded_file"] else None,
+        background_image= bg_image if st.session_state["uploaded_file"] else None,
         #update_streamlit=realtime_update,
-        height=150,
+        height = h if st.session_state["uploaded_file"] else None,
+        width = w if st.session_state["uploaded_file"] else None,
+        #height = st.slider('高度',100,1000,step = 100),
+        #width = st.slider('宽度',100,1000,step = 100),
         drawing_mode=drawing_mode,
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="canvas",
@@ -58,11 +67,31 @@ class Unit:
         self.hp = hp
         self.position = position
     
+def dice_roll():
+    st.subheader("投掷骰子")
+    st.write("点击投掷骰子")
+    result = None
+    if st.button("六面骰"):
+        result = random.randint(1, 6)
+    if st.button("八面骰"):
+        result = random.randint(1, 8)
+    if st.button("十面骰"):
+        result = random.randint(1, 10)
+    if st.button("二十面骰"):
+        result = random.randint(1, 20)
+    if result is not None:
+        st.caption(f'本次投掷骰子的结果是{result}')
+
 def unit_battle():
-    option = st.multiselect(
+    st.subheader('战斗系统')
+    if st.session_state['df'] is None:
+        st.write('目前还没有创建任何角色')
+        return
+    else:
+        option = st.multiselect(
     '请选择战斗人员',
     st.session_state['df']['姓名'])
-    pass
+    
     #hp_now = st.slider('目前血量',0,hp,hp)
 
 unit_list = st.session_state["unit_list"]
@@ -83,7 +112,6 @@ def del_unit(name):
     df = pd.DataFrame([[e.name, e.hp, e.position] for e in st.session_state["unit_list"]], columns=['姓名', 'hp', '职业'])
     st.session_state['df'] = df
 
-
 # 显示单位列表函数
 def show_unit_list():
     if len(st.session_state["unit_list"]) == 0:
@@ -92,11 +120,10 @@ def show_unit_list():
         df = pd.DataFrame([[e.name, e.hp, e.position] for e in st.session_state["unit_list"]], columns=['姓名', 'hp', '职业'])
         st.dataframe(df)
         st.session_state['df'] = df
-        
-        
+          
 # 添加单位界面
 def add_unit_page():
-    st.header('添加新角色')
+    st.subheader('添加新角色')
     name = st.text_input('姓名')
     age = st.number_input('hp', min_value=0, max_value=100)
     position = st.text_input('职业')
@@ -105,44 +132,65 @@ def add_unit_page():
         st.success('添加成功！')
 
 def del_unit_page():
-    st.header('删除角色')
-    if st.session_state['df']['姓名'] is None:
-        st.stop()
-    option = st.selectbox('请选择需要删除的角色',st.session_state['df']['姓名'])
-    if st.button('删除'):
-        del_unit(option)
-        st.success('删除成功！')
+    st.subheader('删除角色')
+    if st.session_state['df'] is None:
+        st.write('目前还没有创建任何角色')
+        return
+    else:
+        option = st.selectbox('请选择需要删除的角色',st.session_state['df']['姓名'])
+        if st.button('删除'):
+            del_unit(option)
+            st.success('删除成功！')
 
 # 显示单位列表界面
 def show_unit_list_page():
-    st.write('角色列表')
+    st.subheader('角色列表')
     st.warning('请勿在添加后刷新页面以避免信息丢失', icon="⚠️")
     show_unit_list()
 
 def upload_map():
+    st.warning('请勿在添加后刷新页面以避免信息丢失', icon="⚠️")
+    st.subheader('地图文件上传')
     uploaded_file= st.file_uploader('地图文件',type=['jpg','png'])
     st.session_state["uploaded_file"] = uploaded_file
-    if st.session_state["uploaded_file"] is not None:
-        st.image(st.session_state["uploaded_file"])
-    st.warning('请勿在添加后刷新页面以避免信息丢失', icon="⚠️")
 
+def save_game():
+    st.title("List to CSV")
+    st.write("Enter your list below:")
+    input_list = st.text_area("List", "")
+    if st.button("Save"):
+        df = pd.DataFrame(input_list.split("\n"), columns=["List"])
+        df.to_csv("output.csv", index=False)
+        output_path = input("Enter output path: ")
+        df.to_csv(output_path, index=False)
+        st.write("List saved to output.csv")
 
-# 主程序
+def load_game():
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.session_state['df1'] = df
+        st.write(st.session_state['df1'])
+
+    # 主程序
 def main():
     st.title('GM辅助系统')
-    menu = ['角色编辑','角色战斗','地图上传','地图批注']
+    menu = ['角色编辑','角色战斗','地图编辑']
     choice = st.sidebar.selectbox('选择菜单', menu)
+    if st.sidebar.button('保存游戏'):
+        save_game()
+    if st.sidebar.button('加载游戏'):
+        load_game()
     if choice == '角色编辑':
         add_unit_page()
         del_unit_page()
         show_unit_list_page()
-    elif choice == '地图上传':
+    elif choice == '地图编辑':
         upload_map()
+        drawing()
     elif choice == '角色战斗':
         unit_battle()
-    else: drawing()
-
-
+        dice_roll()
 
 if __name__ == '__main__':
     main()
