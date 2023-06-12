0e00f9395844d04918b19a3996ce332a436a012e
Osar00 <109191450+Osar00@users.noreply.github.com>
2023-06-13 00:05:13 +0800

    更新了wbs并完善了项目内部的代码逻辑

--- Web/appGM.py
+++ Web/appGM.py
@@ -19,7 +19,7 @@ from streamlit_extras.no_default_selectbox import selectbox
 path = "/Users/oscarchen/Documents/GitHub/py_epi_sys"
 game_name = 'my_game.pickle'
 
-# 所有变量后带0的都是临时变量
+## 所有变量后带0的都是临时变量
 def create_id(name,defined_num):
     st.write(res.universal_id_dict)
     st.subheader('ID设置')
@@ -41,35 +41,73 @@ def create_id(name,defined_num):
             flag = False
     with col1:
         id0 = st.number_input(f'请输入{name}ID:',value=0,min_value=0,max_value=999999,disabled=flag) 
-        if st.button('确认输入ID'):
+        if st.button(f'确认输入{name}ID'):
             id = id0
             idl.pop(0)
             idl.insert(0, id)
             st.success('输入成功！')
     return id
 
-def create_description(name):# 最后别忘了把这个函数复原到每一小节里，来规避异组杂糅的问题
+## 给所有类的描述输入窗口
+def create_description(name):# 规避的方法就是column name不同
     st.subheader(f'{name}描述编辑')
     st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
                     首行信息将用于侧栏展示。''')
-    df = pd.DataFrame([{"Key": "名称", "Value":''},])
+    df = pd.DataFrame([{f"{name}键": "名称", f"{name}值":''},])
     description0 = st.experimental_data_editor(df,num_rows="dynamic",
                                                width=500,height=140)
     description = dict(description0.values[:]) 
     return description
 
+## rad = register_atom_dict，提供事件和原子绑定的窗口
+def event_atom_connection(name):
+    df = pd.DataFrame([{f"{name}事件ID": "", f"{name}原子ID":''},])
+    el_rad0 = st.experimental_data_editor(df,num_rows="dynamic",width=500,height=140)
+    el_rad1 = el_rad0.values[:]
+    el_rad = {}
+    if st.button('确认事件原子连接'):
+        for row in el_rad1:
+            if row[0] not in el_rad:
+                el_rad[row[0]] = [row[1]]
+            else:
+                el_rad[row[0]].append(row[1])
+    return el_rad
+
+## 事件列创建函数
+def create_event_list(name):
+    df0 = pd.DataFrame([{f"{name}事件ID列": ""}])
+    el0 = st.experimental_data_editor(df0,num_rows="dynamic",width=300,height=140)
+    el1 = el0.values[:]
+    event_list0 = el1.tolist()
+    event_list = [x for i in event_list0 for x in i]
+    return event_list
+
+
+
+
+
+
+
+
+
+## 一个自定义保存路径和游戏名的主页
+def home_page():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader('游戏加载与保存')
+        path0 = st.text_input('请输入游戏加载保存路径')
+        name0 = st.text_input('请输入游戏名称')
+        game_name0 = name0 + '.pickle'
+        if st.button('确认输入'):
+            path,game_name = path0,game_name0
+            return path,game_name
+    with col2:
+        st.subheader('事件编辑页面')
 
 ## 从底层重新开始搭建 添加原子的组件
 def add_atom():
-    st.write(res.universal_id_dict)
     id = create_id('原子',0)
-    st.subheader('原子描述编辑')
-    st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
-                    首行信息将用于侧栏展示。''')
-    dfy = pd.DataFrame([{"Key": "名称", "Value":''},])
-    description0 = st.experimental_data_editor(dfy,num_rows="dynamic",
-                                               width=500,height=140)
-    description = dict(description0.values[:]) 
+    description = create_description('原子')
     st.subheader('程序录入')
     with open('model.py', 'r') as f:
         model_data = f.read()
@@ -86,224 +124,136 @@ def add_atom():
         with open(os.path.join(path, game_name), "rb") as f:
             res.universal_id_dict = copy.deepcopy(pickle.load(f))
             res.ond()
-        a = res.atom(func = temp,id = id,description = description)
+        res.atom(func = temp,id = id,description = description)
         with open(os.path.join(path, game_name), "wb") as f:
             pickle.dump(res.universal_id_dict,f)
         center_running()
-        time.sleep(1)
         st.success('原子装载成功！')
+        time.sleep(1)
         st.experimental_rerun()
 
 ## 添加事件的组件
 def add_event():
-    st.write(res.universal_id_dict)
     col1, col2 = st.columns(2)
     with col1:
         id = create_id('事件',1)
-        st.subheader('事件描述编辑')
-        st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
-                        首行信息将用于侧栏展示。''')
-        dfe = pd.DataFrame([{"Key": "名称", "Value":''},])
-        description0 = st.experimental_data_editor(dfe,num_rows="dynamic",
-                                                width=500,height=140)
-        description = dict(description0.values[:]) 
-        if st.button('确认输入事件'):
-            
+        description = create_description('事件')
+        if st.button('确认输入事件'):  
             with open(os.path.join(path, game_name), "rb") as f:
                 res.universal_id_dict = copy.deepcopy(pickle.load(f))
                 res.ond()
-            a = res.event(id=id,description=description)
+            res.event(id=id,description=description)
             with open(os.path.join(path, game_name), "wb") as f:
                 pickle.dump(res.universal_id_dict,f)
-            st.success('事件编写成功！')
             center_running()
+            st.success('事件编写成功！')
             time.sleep(1)
             st.experimental_rerun()
     with col2:
+        st.subheader('展示事件列栏目')
+        selectbox([])
         pass
 
-## 添加动作的组件
+## 添加行动的组件
 def add_action():
-    st.write(res.universal_id_dict)
     col1, col2 = st.columns(2)
     with col1:
         id = create_id('行动',2)
-        st.subheader('行动描述编辑')
-        st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
-                        首行信息将用于侧栏展示。''')
-        dfx = pd.DataFrame([{"Key": "名称", "Value":''},])
-        description0 = st.experimental_data_editor(dfx,num_rows="dynamic",
-                                                width=500,height=140)
-        description = dict(description0.values[:]) 
+        description = create_description('行动')
     with col2:
         st.subheader('行动注册')
-        df = pd.DataFrame([{"Event ID": "", "Atom ID":''},])
-        el_rad0 = st.experimental_data_editor(df,num_rows="dynamic",
-                                                width=500,height=140)
-        el_rad1 = el_rad0.values[:]
-        el_rad = {}
-        if st.button('确认事件原子连接'):
-            for row in el_rad1:
-                if row[0] not in el_rad:
-                    el_rad[row[0]] = [row[1]]
-                else:
-                    el_rad[row[0]].append(row[1])
+        el_rad = event_atom_connection('行动')
         st.subheader('注册事件列')
-        df0 = pd.DataFrame([{"Event ID List": ""}])
-        el0 = st.experimental_data_editor(df0,num_rows="dynamic",
-                                                width=300,height=140)
-        el1 = el0.values[:]
-        event_list0 = el1.tolist()
-        event_list = [x for i in event_list0 for x in i]
+        event_list = create_event_list('行动')
         if st.button('确认输入行动'):
-            
-            
             with open(os.path.join(path, game_name), "rb") as f:
                 res.universal_id_dict = copy.deepcopy(pickle.load(f))
                 res.ond()
-            a = res.action(reg_atom_dict=el_rad,event_list=event_list,id=id,
-                        description=description)
+            res.action(reg_atom_dict=el_rad,event_list=event_list,id=id,description=description)
             with open(os.path.join(path, game_name), "wb") as f:
                 pickle.dump(res.universal_id_dict,f)
             center_running()
-            time.sleep(1)
             st.success('行动创建成功！')
+            time.sleep(1)
             st.experimental_rerun()
 
 ## 属性
 def add_attribution():
-    st.write(res.universal_id_dict)
     col1, col2 = st.columns(2)
     with col1:
         id = create_id('属性',3)
-        st.subheader('属性描述编辑')
-        st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
-                        首行信息将用于侧栏展示。''')
-        dfas = pd.DataFrame([{"Key": "名称", "Value":''},])
-        description0 = st.experimental_data_editor(dfas,num_rows="dynamic",
-                                                width=500,height=140)
-        description = dict(description0.values[:]) 
+        description = create_description('属性')
         st.subheader('属性注册')
-        df0 = pd.DataFrame([{"Event ID": "", "Atom ID":''},])
-        el_rad0 = st.experimental_data_editor(df0,num_rows="dynamic",
-                                                width=500,height=140)
-        el_rad1 = el_rad0.values[:]
-        el_rad = {}
-        if st.button('确认事件原子连接'):
-            for row in el_rad1:
-                if row[0] not in el_rad:
-                    el_rad[row[0]] = [row[1]]
-                else:
-                    el_rad[row[0]].append(row[1])
+        el_rad = event_atom_connection('属性')
     with col2:
         valuable = st.checkbox('此属性为数值类属性')
         if valuable:
-            flag = True
             limit_num = st.number_input('上限',min_value=1,max_value=999999)
             value_num = st.number_input('数值',min_value=0,max_value=limit_num)
             st.subheader('数值变动事件列')
-            df1 = pd.DataFrame([{"Event ID List on Value Change": ""}])
-            el0 = st.experimental_data_editor(df1,num_rows="dynamic",
-                                                    width=300,height=140)
-            el_vc = el0.values[:]
-            event_list_vc0 = el_vc.tolist()
-            event_list_vc = [x for i in event_list_vc0 for x in i]
+            event_list_vc = create_event_list('数值变动')
             st.subheader('上限变动事件列')
-            df1 = pd.DataFrame([{"Event ID List on Limit Change": ""}])
-            el1= st.experimental_data_editor(df1,num_rows="dynamic",
-                                                    width=300,height=140)
-            el_lc = el1.values[:]
-            event_list_lc0 = el_lc.tolist()
-            event_list_lc = [x for i in event_list_lc0 for x in i]
+            event_list_lc = create_event_list('上限变动')
             if st.button('确认输入属性'):
                 # 待调整
                 with open(os.path.join(path, game_name), "rb") as f:
                     res.universal_id_dict = copy.deepcopy(pickle.load(f))
                     res.ond()
-                a = res.attribution(valuable = flag,limit = limit_num, value = value_num,
-    reg_atom_dict=el_rad,owner=None,event_list_on_value_change=event_list_vc,
-    event_list_on_limit_change=event_list_lc,attach_ctn=None,id=id,description=description)
+                res.attribution(valuable = True,limit = limit_num, value = value_num,reg_atom_dict=el_rad,owner=None,event_list_on_value_change=event_list_vc,event_list_on_limit_change=event_list_lc,attach_ctn=None,id=id,description=description)
                 with open(os.path.join(path, game_name), "wb") as f:
                     pickle.dump(res.universal_id_dict,f)
-            
                 center_running()
+                st.success('数值属性创造成功！')
                 time.sleep(1)
-                st.success('属性创造成功！')
                 st.experimental_rerun()
         else:
-            flag = False
             if st.button('确认输入属性'):
-
                 with open(os.path.join(path, game_name), "rb") as f:
                     res.universal_id_dict = copy.deepcopy(pickle.load(f))
                     res.ond()
-                a = res.attribution(valuable = flag, reg_atom_dict=el_rad,owner=None,
-                                    attach_ctn=None,id=id,description=description)
-
+                res.attribution(valuable = False, reg_atom_dict=el_rad,owner=None,attach_ctn=None,id=id,description=description)
                 with open(os.path.join(path, game_name), "wb") as f:
                     pickle.dump(res.universal_id_dict,f)
-                
                 center_running()
+                st.success('非数值属性创造成功！')
                 time.sleep(1)
-                st.success('属性创造成功！')
                 st.experimental_rerun()
 
 # 角色
 def add_unit():
-    st.write(res.universal_id_dict)
     col1, col2 = st.columns(2)
     with col1:
         id = create_id('角色',4)
-        st.subheader('角色描述编辑')
-        st.caption('''编辑前请先按左上角后按delete清空，表格大小可拖动调节，输入内容可单击编辑。
-                        首行信息将用于侧栏展示。''')
-        dfu = pd.DataFrame([{"Key": "名称", "Value":''},])
-        description0 = st.experimental_data_editor(dfu,num_rows="dynamic",
-                                                width=500,height=140)
-        description = dict(description0.values[:]) 
-        # init_attribution
-        # attribution_dict
+        description = create_description('角色')
+        # init_attribution / attribution_dict
         if st.button('确认输入角色'):
             with open(os.path.join(path, game_name), "rb") as f:
                 res.universal_id_dict = copy.deepcopy(pickle.load(f))
                 res.ond()
-            a = res.unit(id=id,description=description)
+            res.unit(id=id,description=description)
             with open(os.path.join(path, game_name), "wb") as f:
                 pickle.dump(res.universal_id_dict,f)
-            st.success('角色编写成功！')
             center_running()
+            st.success('角色编写成功！')
             time.sleep(1)
             st.experimental_rerun()
     with col2:
-        st.subheader('角色模版')
-        unit_menu=[]
-        for _ in res.universal_id_dict.keys():
-            if _[-1] == 4:
-                menu_id = res.universal_id_dict[_].id
-                menu_name = res.universal_id_dic[_].description
-                unit_menu.append([menu_id,menu_name])
-        a0 = selectbox('拷贝角色',unit_menu)
+        st.subheader('角色模版') # 和许睿确定拷贝的细则
+        a0 = file_seeker('角色拷贝',4)
         if st.button('确认拷贝角色'):
             with open(os.path.join(path, game_name), "rb") as f:
                 res.universal_id_dict = copy.deepcopy(pickle.load(f))
                 res.ond()
-            a = copy.deepcopy(res.universal_id_dict[a0[0]])
+            res.unit(copy.deepcopy(res.universal_id_dict[a0[0]]))
             with open(os.path.join(path, game_name), "wb") as f:
                 pickle.dump(res.universal_id_dict,f)
-            st.success('角色编写成功！')
             center_running()
+            st.success('角色拷贝成功！')
             time.sleep(1)
             st.experimental_rerun()
-        
         # selectbox('请选择角色模版',unit_menu)
         # 角色模板
         # 角色属性初始化
-        
-    
-
-
-
-
 
 def upload_map():
     st.warning('请勿在添加后刷新页面以避免信息丢失', icon="⚠️")
@@ -344,22 +294,16 @@ def add_map():
         point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
         key="canvas",
     )
-
 # 游戏界面
 def battle():
     col1, col2 = st.columns(2)
     with col1:
         st.subheader('请选择行动')
-        action_menu=[]
-        for _ in res.universal_id_dict.keys():
-            if _[-1] == 2:
-                menu_id = res.universal_id_dict[_].id
-                menu_name = res.universal_id_dic[_].description
-                action_menu.append([menu_id,menu_name])
-        a0 = selectbox('开始行动（在此之前，确保你已经创建了事件和动作）',action_menu)
-        a = res.universal_id_dict[a0[0]]
+        a0 = file_seeker('开始行动（在此之前，确保你已经创建了事件和动作）',2)
+        if a0 is not None:
+            a = res.universal_id_dict[a0[0]]
         action_owner_id = st.number_input('绑定实施者',0,999999)
-        dfao = pd.DataFrame([{"world_status": "", "传入id":''},])
+        dfao = pd.DataFrame([{"世界状态": "", "传入ID":''},])
         description0 = st.experimental_data_editor(dfao,num_rows="dynamic",
                                                 width=500,height=140)
         description_oid = dict(description0.values[:]) 
@@ -389,22 +333,16 @@ def battle():
         #if selected_action is not None:
             #file = f'{selected_action["id"]}.pickle'
 
-    
-        
-
-
-
-
-
 # 文件查找函数
 def file_seeker(name,defined_num):
     menu=[]
     for _ in res.universal_id_dict.keys():
-        if _%100 == defined_num:
+        if _ % 100 == defined_num:
             menu_id = res.universal_id_dict[_].id
             menu_name = res.universal_id_dict[_].description
             menu.append([menu_id,menu_name])
-    st.sidebar.selectbox(f'{name}',menu)
+    a0 = selectbox(f'{name}',menu)
+    return a0
 
 # 主程序
 def main():
@@ -419,6 +357,7 @@ def main():
         file_seeker("角色",4)
     if selected == '首页':
         colored_header(label="GM辅助系统",description="一款自定义TRPG辅助工具系统，首页具有刷新功能",color_name="violet-70",)
+        home_page() # 发布后启用的功能
         res.universal_id_dict={}
         with open(os.path.join(path, game_name), "wb") as f:
             pickle.dump(res.universal_id_dict,f)
